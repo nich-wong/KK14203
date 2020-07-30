5b6b5bdc07dd008e12ffa69e50279e18a14aff01
nich-wong <67786867+nich-wong@users.noreply.github.com>
2020-07-30 15:39:53 +0800

    Add files via upload

---
+++ Lab 7/Lab7.java
@@ -0,0 +1,40 @@
+/*	NICHOLAS WONG
+ * 	BI19110023
+ * 	KK14203 OOP (SECTION 1) HCOO
+ * 	LAB 7
+ */
+
+public class Lab7 {
+	
+	public static void main(String[] args) {
+		
+		new MyFrame();
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
